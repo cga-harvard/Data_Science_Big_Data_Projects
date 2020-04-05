26cd1d8f2356ff04d2a6105942bb433b20491362
dkakkar <devikakakkar29@gmail.com>
2020-04-05 16:07:03 -0400

    Updated scripts
    Deleted

--- scripts/postgis.py
+++
@@ -1,4 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(dbname=postgres, port=7435, user=postgres,password=postgres, host=localhost)
-cur = conn.cursor()
-print("Connected")
--- scripts/scripts/setup_restore_db.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' port='7548'")
-except:
-    print "I am unable to connect to the database"
