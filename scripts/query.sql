5ed336c9e3f88e10899531275952b24038f2d7d4
dkakkar <devikakakkar29@gmail.com>
2020-09-01 15:00:37 -0400

    Update query.sql

--- scripts/query.sql
+++ scripts/query.sql
@@ -1,4 +1,4 @@
-Create table results as (SELECT  a.id as id, b.filedate AS day, 
+Create table results as (SELECT  a.id as id, regexp_replace(CAST(b.filedate AS TEXT),'-','','g') AS day, 
 ST_VALUE(b.rast, ST_Transform(ST_SetSRID( ST_Point(a.longitude, a.latitude), 4326), 4269)) as tmax 
 from addresses a 
 inner join tmaxunion b 
