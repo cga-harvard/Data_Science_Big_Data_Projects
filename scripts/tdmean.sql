a06c5803a56a37a270ef82d89522a27935040819
dkakkar <devikakakkar29@gmail.com>
2020-09-10 22:51:20 -0400

    Update tdmean.sql

--- scripts/tdmean.sql
+++ scripts/tdmean.sql
@@ -8,6 +8,6 @@ ST_Intersects(ST_Envelope(b.rast), ST_Transform(ST_SetSRID( ST_Point(a.longitude
 AND startdate <= filedate AND enddate >= filedate);
 Alter table results add column RH float;
 Alter table results add column AH float;  
-Update results SET RH=;
-Update results SET AH=;                 
+Update results SET RH=100*(exp((17.625*tdmean)/(243.04+tdmean))/exp((17.625*tmean)/(243.04+tmean)));
+Update results SET AH={6.112*exp[(17.67*tmean)/(tmean +243.5)] * RH * 2.1674}/{273.15+ tmean};                 
 Copy(Select * from results) TO '/n/holyscratch01/cga/dkakkar/data/results.csv';
