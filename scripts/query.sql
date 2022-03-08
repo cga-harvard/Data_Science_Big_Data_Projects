edfd0e8f5592717643e71b291a2f413f8c99847c
dkakkar <devikakakkar29@gmail.com>
2022-03-08 13:43:47 -0500

    Delete query.sql

--- scripts/query.sql
+++
@@ -1,8 +0,0 @@
-Create table results as (SELECT  a.id as id, regexp_replace(CAST(b.filedate AS TEXT),'-','','g') AS day, 
-ST_VALUE(b.rast, ST_Transform(ST_SetSRID( ST_Point(a.longitude, a.latitude), 4326), 4269)) as tmax 
-from addresses a 
-inner join tmaxunion b 
-ON
-ST_Intersects(ST_Envelope(b.rast), ST_Transform(ST_SetSRID( ST_Point(a.longitude, a.latitude), 4326), 4269)) = 't'
-AND startdate <= filedate AND enddate >= filedate);
-Copy(Select * from results) TO '/n/holyscratch01/cga/dkakkar/data/results.csv';
