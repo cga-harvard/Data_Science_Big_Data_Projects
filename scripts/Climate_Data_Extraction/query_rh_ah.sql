3cb4f242264a14426f6e7e6ba41b545d8fcc9c0b
dkakkar <devikakakkar29@gmail.com>
2022-03-08 13:45:01 -0500

    Add files via upload

---
+++ scripts/Climate_Data_Extraction/query_rh_ah.sql
@@ -0,0 +1,25 @@
+Drop table IF exists resultstdmean;
+Drop table IF exists resultstmean;
+Drop table IF exists temp;
+
+Create table resultstdmean (address_id character varying(255),day date, tdmean FLOAT);
+
+
+\copy resultstdmean from '/n/holyscratch01/cga/dkakkar/data/tdmean_results.csv' (FORMAT csv, delimiter ',', CSV HEADER);
+
+Create table resultstmean (address_id character varying(255),day date, tmean FLOAT);
+
+
+\copy resultstmean from '/n/holyscratch01/cga/dkakkar/data/tmean_results.csv' ((FORMAT csv, delimiter ',', CSV HEADER);
+
+
+Create table temp as (Select a.address_id, a.day, a.tdmean, b.tmean from resultstdmean a inner join resultstmean b on a.address_id=b.address_id AND a.day=b.day);
+
+Alter table temp add column RH FLOAT;
+
+Alter table temp add column AH FLOAT;
+
+Update temp set RH=100*(EXP((17.625*tdmean)/(243.04+tdmean))/EXP((17.625*tmean)/(243.04+tmean)));
+Update temp SET AH=(6.112*EXP((17.67*tmean)/(tmean +243.5)) * RH * 2.1674)/(273.15+ tmean);
+
+\copy (Select address_id, day, RH, AH from temp) to '/n/holyscratch01/cga/dkakkar/data/results_rh_ah_test.csv';
