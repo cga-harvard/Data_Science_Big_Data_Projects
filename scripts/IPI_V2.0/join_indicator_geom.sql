e915de8415b7b09fd5e87723ad03aa0a37cef8d2
Jack-Hayes <109687530+Jack-Hayes@users.noreply.github.com>
2024-06-10 09:53:31 -0400

    Delete scripts/IPI_V2.0/join_indicator_geom.sql

--- scripts/IPI_V2.0/join_indicator_geom.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Indicator_District_Data_Geom AS
-SELECT Indicator_District_Data.*, IPI722DistrictShapefile.Geom
-FROM Indicator_District_Data
-JOIN IPI722DistrictShapefile
-ON Indicator_District_Data.District_id = IPI722DistrictShapefile.DistrictID;
